2b13400282a82e6e03c18eb69994f53e36cca59a
Isbat Uzzin N <isbatuzzin@gmail.com>
2024-07-08 16:30:44 +0700

    assigmet sql

--- sql_isbat.sql
+++ sql_isbat.sql
@@ -5,7 +5,6 @@ create database sekolah;
 \c sekolah;
 
 -- create tabel student
-
 create table student (
     id int not null AUTO_INCREMENT, 
     first_name varchar(10) not null, 
@@ -18,3 +17,23 @@ create table student (
     created_at timestamp with time zone UTC default now,
     PRIMARY KEY (Personid)
 );
+
+-- store procedure/function  menghitung jumlah film  berdasarkan genre
+
+CREATE OR REPLACE FUNCTION hitung_jumlah_film_berdasarkan_genre(
+    genre_id_param INT
+) RETURNS INT AS $$
+DECLARE
+    jumlah_film INT;
+BEGIN
+    jumlah_film=(select count (mg.gen_id) from movie m
+    right join movie_genres mg on m.mov_id=mg.mov_id
+    group by mg.gen_id
+    having mg.gen_id = genre_id_param);
+
+    RETURN jumlah_film;
+END;
+$$ LANGUAGE plpgsql;
+
+--call function
+select hitung_jumlah_film_berdasarkan_genre(1002) a;
